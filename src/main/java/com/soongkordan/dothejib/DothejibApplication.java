d7305f47c55cea8c6aad775f089a3d8bb61693f4
LouiMinister <louiminister@gmail.com>
2022-07-14 17:15:12 +0900

    Init Spring Boot JPA Backend Project

---
+++ src/main/java/com/soongkordan/dothejib/DothejibApplication.java
@@ -0,0 +1,13 @@
+package com.soongkordan.dothejib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DothejibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DothejibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soongkordan/dothejib/DothejibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soongkordan.dothejib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DothejibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
