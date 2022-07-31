ff1b4765d6c1dd23b49c2b48868a04fc1a04a141
LouiMinister <louiminister@gmail.com>
2022-07-31 18:34:32 +0900

    feat: TodoRepository 구현

---
+++ src/main/java/com/soongkordan/dothejib/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.soongkordan.dothejib.repository;
+
+import com.soongkordan.dothejib.domain.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    List<Todo> findByFamilyId(Long familyId);
+}
