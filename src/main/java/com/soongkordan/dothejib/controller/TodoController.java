36d44045060c5cefb4450545657f96ce8742b0b8
jwPark <1211abc@naver.com>
2022-08-05 17:54:44 +0900

    style: todoController 코드 포맷팅

--- src/main/java/com/soongkordan/dothejib/controller/TodoController.java
+++ src/main/java/com/soongkordan/dothejib/controller/TodoController.java
@@ -33,29 +33,42 @@ public class TodoController {
     private final FamilyService familyService;
     private final FamilyMemberService familyMemberService;
 
-    //TODO: 너무 길어서 코드 리팩토링 필요(검수 + 반환 한 번에 묶기).....
-    /* Todo 생성*/
+    //TODO: 코드가 너무 길어서 리팩토링 필요
+
+    /*
+    POST    Todo 생성
+    GET     Todo 단일 조회
+    GET     Todo 목록 조회(가족단위 )
+    GET     Todo 목록 조회(가족구성원 단위 )
+    PUT     Todo 수정
+    DELETE  Todo 삭제
+    POST    Todo Check( 완료 시간 생성 )
+    DELETE  Todo UnCheck( 완료 시간 삭제 )
+     */
+
+    /*Todo 생성*/
     @PostMapping(path = "/Todo/new",produces =  APPLICATION_JSON_VALUE)
-    public ResponseEntity<? extends BasicResponse> save(
+    public ResponseEntity<? extends BasicResponse> saveTodo(
             @RequestBody @Valid SaveRequest request
     ){
+        //family,publisher id 유효 여부 판단
         Optional<Family> family = familyService.findOne(request.getFamilyId());
-        if(!family.isPresent()){
+        if(!family.isPresent())
             return ResponseEntity.status(HttpStatus.NOT_FOUND) //return : 404
                     .body(new ErrorResponse("일치하는 가족 정보가 없습니다. id를 확인해주세요."));
-        }
+
         Optional<FamilyMember> publisher = familyMemberService.findOne(request.getPublisherId());
-        if(!publisher.isPresent()){
+        if(!publisher.isPresent())
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ErrorResponse("일치하는 발행자 정보가 없습니다. id를 확인해주세요."));
-        }
+
+        //personInCharge id 입력 여부 판단 후 Todo 객체 생성
         Todo todo;
         if(request.getPersonInChargeId()!=null){
             Optional<FamilyMember> personInCharge = familyMemberService.findOne(request.getPersonInChargeId());
-            if(!personInCharge.isPresent()){
+            if(!personInCharge.isPresent())
                 return ResponseEntity.status(HttpStatus.NOT_FOUND)
                         .body(new ErrorResponse("일치하는 담당자 정보가 없습니다. id를 확인해주세요."));
-            }
             todo = Todo.createTodo(family.get(),publisher.get(),personInCharge.get(),
                     request.getTitle(),request.getDifficulty(),request.getContent(),request.getEndAt());
         }
@@ -63,58 +76,54 @@ public class TodoController {
             todo = Todo.createTodo(family.get(),publisher.get(),null,
                     request.getTitle(),request.getDifficulty(),request.getContent(),request.getEndAt());
         }
+
         Long savedId = todoService.save(todo);
-        return ResponseEntity.ok()
+        return ResponseEntity.ok() //return : 200 + todoId
                 .body(new CommonResponse<CommonDTO.IdResponse>(
                         new CommonDTO.IdResponse(savedId)));
     }
 
-    /*Todo 완료 시간 생성 (Check)*/
-    @PostMapping(path = "/Todo/complete/{id}",produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<? extends BasicResponse> Complete(
-            @RequestBody @Valid CompleteRequest request,
-            @PathVariable("id") Long todoId
+    /*Todo 단일 조회*/
+    @GetMapping(path = "/Todo/{id}",produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<? extends BasicResponse> getOneTodo(
+            @PathVariable("id") Long todoId,
+            @RequestBody @Valid getTodoRequest request
     ){
+        //todoId 유효 여부 판단
         Optional<Todo> todo = todoService.findOne(todoId);
-        if(!todo.isPresent()){
+        if(!todo.isPresent())
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ErrorResponse("일치하는 할 일 정보가 없습니다. id를 확인해주세요."));
-        }
-        todoService.completeTodo(todoId,request.getCompletedAt());
-        return ResponseEntity.status(HttpStatus.CREATED).build(); //return : 201
-    }
 
-    /*Todo 완료 시간 삭제(UnChecked)*/
-    @DeleteMapping(path = "/Todo/Complete/{id}",produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<? extends BasicResponse> InComplete(
-            @PathVariable("id") Long todoId
-    ){
-        Optional<Todo> todo = todoService.findOne(todoId);
-        if(!todo.isPresent()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new ErrorResponse("일치하는 할 일 정보가 없습니다. id를 확인해주세요."));
-        }
-        todoService.inCompleteTodo(todoId);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); //return : 204
+        Todo Todo = todo.get();
+        return ResponseEntity.ok()
+                .body(new CommonResponse<getTodoInfoResponse>(
+                        new getTodoInfoResponse(
+                                Todo.getId(),Todo.getPublisher().getId(),Todo.getPersonInCharge().getId(),Todo.getTitle(),
+                                Todo.getContent(),Todo.getDifficulty(),Todo.getCompletedAt())));
     }
 
     /*Todo 목록 조회(가족단위)*/
     @GetMapping(path = "/FamilyTodo/{id}",produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<? extends BasicResponse> getAllTodo(
+    public ResponseEntity<? extends BasicResponse> getFamilyTodo(
             @PathVariable("id") Long familyId,
             @RequestBody @Valid getTodoRequest request
     ){
+        //familyId 유효 여부 판단
         Optional<Family> family = familyService.findOne(familyId);
         if(!family.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND) //return : 404
                     .body(new ErrorResponse("일치하는 가족 정보가 없습니다. id를 확인해주세요."));
         }
+
+        //List<Entity> -> List<DTO>
         List<Todo> AllTodo = todoService.findByFamilyIdAndEndAt(familyId, request.getEndAt());
         List<getTodoInfoResponse> todoInfo = AllTodo.stream()
                 .map(t -> new getTodoInfoResponse(
                         t.getId(), t.getPublisher().getId(),t.getPersonInCharge().getId(),t.getTitle()
                         ,t.getContent(), t.getDifficulty(),t.getCompletedAt()))
                 .collect(Collectors.toList());
+
         return ResponseEntity.ok() //return : 200 + todoInfoList
                 .body(new CommonResponse<List>(todoInfo));
     }
@@ -125,33 +134,23 @@ public class TodoController {
             @PathVariable("id") Long personInChargeId,
             @RequestBody @Valid getTodoRequest request
     ){
+        //PersonInChargeId 유효 여부 판단
         Optional<FamilyMember> personInCharge = familyMemberService.findOne(personInChargeId);
         if(!personInCharge.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND) //return : 404
                     .body(new ErrorResponse("일치하는 담당자 정보가 없습니다. id를 확인해주세요."));
         }
+
+        //List<Entity> -> List<DTO>
         List<Todo> AllTodo = todoService.findByPersonInChargeIdAndEndAt(personInChargeId,request.getEndAt());
         List<getTodoInfoResponse> todoInfo = AllTodo.stream()
                 .map(t -> new getTodoInfoResponse(
                         t.getId(), t.getPublisher().getId(),t.getPersonInCharge().getId(),t.getTitle()
                         ,t.getContent(), t.getDifficulty(),t.getCompletedAt()))
                 .collect(Collectors.toList());
+
         return ResponseEntity.ok() //return : 200 + todoInfoList
                 .body(new CommonResponse<List>(todoInfo));
-    }
-
-    /*Todo 삭제*/
-    @DeleteMapping(path = "/Todo/{id}", produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<? extends BasicResponse> deleteTodo(
-            @PathVariable("id") Long todoId
-    ){
-        Optional<Todo> todo = todoService.findOne(todoId);
-        if(!todo.isPresent()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new ErrorResponse("일치하는 할 일 정보가 없습니다. id를 확인해주세요."));
-        }
-        todoService.deleteOne(todoId);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); //return : 204
     }
 
     /*Todo 수정*/
@@ -160,17 +159,20 @@ public class TodoController {
             @PathVariable("id") Long todoId,
             @RequestBody @Valid modifyRequest request
     ) {
+        //todoId 유효 여부 판단
         Optional<Todo> todo = todoService.findOne(todoId);
         if (!todo.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ErrorResponse("일치하는 할 일 정보가 없습니다. id를 확인해주세요."));
         }
+
+        //personInCharge Id 유효 여부 판단 및 수정 메소드 호출
         if (request.getPersonInChargeId() != null) {
             Optional<FamilyMember> personInCharge = familyMemberService.findOne(request.getPersonInChargeId());
-            if (!personInCharge.isPresent()) {
+            if (!personInCharge.isPresent())
                 return ResponseEntity.status(HttpStatus.NOT_FOUND)
                         .body(new ErrorResponse("일치하는 담당자 정보가 없습니다. id를 확인해주세요."));
-            }
+
             todoService.modifyTodo(todoId, personInCharge.get(), request.getTitle(),
                     request.getContent(), request.getDifficulty(), request.getEndAt());
         }
@@ -178,26 +180,58 @@ public class TodoController {
             todoService.modifyTodo(todoId, null, request.getTitle(),
                     request.getContent(), request.getDifficulty(), request.getEndAt());
         }
-        return ResponseEntity.status(HttpStatus.CREATED).build(); //return : 201
+
+        return ResponseEntity.ok().build(); //return : 200
     }
 
-    /*Todo 조회*/
-    @GetMapping(path = "/Todo/{id}",produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<? extends BasicResponse> getTodo(
-            @PathVariable("id") Long todoId,
-            @RequestBody @Valid getTodoRequest request
+    /*Todo 삭제*/
+    @DeleteMapping(path = "/Todo/{id}", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<? extends BasicResponse> deleteTodo(
+            @PathVariable("id") Long todoId
     ){
+        //todoId 유효 여부 판단
         Optional<Todo> todo = todoService.findOne(todoId);
         if(!todo.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ErrorResponse("일치하는 할 일 정보가 없습니다. id를 확인해주세요."));
         }
 
-        Todo TodoG = todo.get();
-        return ResponseEntity.ok()
-                .body(new CommonResponse<getTodoInfoResponse>(
-                       new getTodoInfoResponse(
-                            TodoG.getId(),TodoG.getPublisher().getId(),TodoG.getPersonInCharge().getId(),TodoG.getTitle(),
-                            TodoG.getContent(),TodoG.getDifficulty(),TodoG.getCompletedAt())));
+        // 삭제
+        todoService.deleteOne(todoId);
+        return ResponseEntity.ok().build(); //return : 200
     }
+
+    /*Todo Check( 완료 시간 생성 )*/
+    @PostMapping(path = "/Todo/complete/{id}",produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<? extends BasicResponse> CompleteTodo(
+            @RequestBody @Valid CompleteRequest request,
+            @PathVariable("id") Long todoId
+    ){
+        //todoId가 유효한지 확인
+        Optional<Todo> todo = todoService.findOne(todoId);
+        if(!todo.isPresent())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponse("일치하는 할 일 정보가 없습니다. id를 확인해주세요."));
+
+        //완료 시간(LocalDate) 생성
+        todoService.completeTodo(todoId,request.getCompletedAt());
+        return ResponseEntity.ok().build(); //return : 200
+    }
+
+    /*Todo UnCheck( 완료 시간 삭제 )*/
+    @DeleteMapping(path = "/Todo/complete/{id}",produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<? extends BasicResponse> InCompleteTodo(
+            @PathVariable("id") Long todoId
+    ){
+        //todoId가 유효한지 확인
+        Optional<Todo> todo = todoService.findOne(todoId);
+        if(!todo.isPresent())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponse("일치하는 할 일 정보가 없습니다. id를 확인해주세요."));
+
+        //완료 시간(LocalDate) 삭제
+        todoService.inCompleteTodo(todoId);
+        return ResponseEntity.ok().build(); //return : 200
+    }
+
 }
