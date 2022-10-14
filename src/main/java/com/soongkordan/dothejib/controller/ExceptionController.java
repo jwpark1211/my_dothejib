57d3bad1f4ae362350f188ddb01023ec82f4fdd1
jwPark <1211abc@naver.com>
2022-10-14 17:29:18 +0900

    feat: ExceptionHandler 구현

---
+++ src/main/java/com/soongkordan/dothejib/controller/ExceptionController.java
@@ -0,0 +1,31 @@
+package com.soongkordan.dothejib.controller;
+
+import com.soongkordan.dothejib.controller.exception.BasicResponse;
+import com.soongkordan.dothejib.controller.exception.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionController {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<? extends BasicResponse> IllegalArgumentHandler(IllegalArgumentException e){
+        log.warn("-----IllegalArgumentException-----");
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<? extends BasicResponse> RunTimeHandler(RuntimeException e){
+        log.warn("-----RunTimeException-----");
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(e.getMessage()));
+    }
+}
