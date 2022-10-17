cd25ef6ded8dc0bac8fc46d9c222833b95795d5b
jwPark <1211abc@naver.com>
2022-10-17 18:33:19 +0900

    test : familyMemberServiceTest 수정

--- src/test/java/com/soongkordan/dothejib/service/FamilyMemberServiceTest.java
+++ src/test/java/com/soongkordan/dothejib/service/FamilyMemberServiceTest.java
@@ -1,17 +1,23 @@
 package com.soongkordan.dothejib.service;
 
+import com.soongkordan.dothejib.controller.dto.FamilyDTO;
+import com.soongkordan.dothejib.controller.dto.FamilyMemberDTO;
 import com.soongkordan.dothejib.domain.Authority;
 import com.soongkordan.dothejib.domain.Family;
 import com.soongkordan.dothejib.domain.FamilyMember;
 import com.soongkordan.dothejib.domain.Member;
+import com.soongkordan.dothejib.repository.FamilyRepository;
+import com.soongkordan.dothejib.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import static com.soongkordan.dothejib.controller.dto.FamilyMemberDTO.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -19,108 +25,91 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FamilyMemberServiceTest {
 
     @Autowired FamilyMemberService familyMemberService;
-    @Autowired FamilyService familyService;
-    @Autowired MemberService memberService;
+    @Autowired FamilyRepository familyRepository;
+    @Autowired MemberRepository memberRepository;
+
+    Member member1 = Member.builder().email("test1@email").password("password").build();
+    Member member2 = Member.builder().email("test2@email").password("password").build();
+    Member member3 = Member.builder().email("test3@email").password("password").build();
+    Family family1 = Family.builder().name("family1").createdAt(LocalDateTime.now()).build();
+    Family family2 = Family.builder().name("family2").createdAt(LocalDateTime.now()).build();
 
     @Test
     void 가족구성원_저장(){
-        //given
-        Member member = getMember("email@email.com");
-        Family family = getFamily("name");
-
-        //when
-        FamilyMember familyMember = getFamilyMember(member, family, "nameFm");
-        FamilyMember findFamilyMember = familyMemberService.findOne(familyMember.getId()).get();
-
+        //given & when
+        IdResponse idResponse = getFamilyMember("name1",member1,family1);
         //then
-        assertEquals(familyMember,findFamilyMember);
+        assertEquals("name1",familyMemberService.getFamilyMemberInfoWithId(idResponse.getId()).getName());
     }
 
     @Test
     void 가족아이디로_가족구성원_조회(){
         //given
-        Member member1 = getMember("email1@email.com");
-        Member member2 = getMember("email2@email.com");
-        Family family1 = getFamily("name1");
-        Family family2 = getFamily("name2");
-        FamilyMember familyMember1 = getFamilyMember(member1, family1, "nameFm1");
-        FamilyMember familyMember2 = getFamilyMember(member2, family1, "nameFm2");
+        IdResponse idResponse1 = getFamilyMember("name1",member1,family1);
+        IdResponse idResponse2 = getFamilyMember("name2",member2,family1);
 
         //when
-        List<FamilyMember> familyMembers = familyMemberService.findByFamilyId(family1.getId());
+        List<FamilyMemberInfoResponse> response = familyMemberService.getFamilyMembersInfoWithFamilyId(family1.getId());
 
         //then
-        assertEquals(2,familyMembers.size());
+        assertEquals(2,response.size());
+    }
+
+    @Test
+    void 가족아이디와_유저아이디로_가족구성원_조회(){
+        //given
+        IdResponse idResponse1 = getFamilyMember("name1",member1,family1);
+        IdResponse idResponse2 = getFamilyMember("name2",member2,family1);
+
+        //when
+        FamilyMemberInfoResponse response =
+                familyMemberService.getFamilyMemberInfoWithFamilyIdAndMemberId(family1.getId(),member2.getId());
+
+        //then
+        assertEquals(response.getName(),"name2");
     }
 
     @Test
     void 유저아이디로_가족구성원_조회(){
         //given
-        Member member1 = getMember("email1@email.com");
-        Member member2 = getMember("email2@email.com");
-        Family family1 = getFamily("name1");
-        Family family2 = getFamily("name2");
-        FamilyMember familyMember1 = getFamilyMember(member1, family1, "nameFm1");
-        FamilyMember familyMember2 = getFamilyMember(member2, family1, "nameFm2");
+        IdResponse idResponse1 = getFamilyMember("name1",member1,family1);
+        IdResponse idResponse2 = getFamilyMember("name2",member1,family2);
+        IdResponse idResponse3 = getFamilyMember("name3",member2,family2);
 
         //when
-        List<FamilyMember> familyMembers = familyMemberService.findByMemberId(member2.getId());
+        List<FamilyMemberInfoResponse> response = familyMemberService.getFamilyMemberInfoWithMemberId(member1.getId());
 
         //then
-        assertEquals(1,familyMembers.size());
-    }
-
-    @Test
-    void 유저아이디와_가족아이디로_가족구성원_조회(){
-        //given
-        Member member1 = getMember("email1@email.com");
-        Member member2 = getMember("email2@email.com");
-        Family family1 = getFamily("name1");
-        Family family2 = getFamily("name2");
-        FamilyMember familyMember1 = getFamilyMember(member1, family1, "nameFm1");
-        FamilyMember familyMember2 = getFamilyMember(member2, family1, "nameFm2");
-
-        //when
-        FamilyMember find =
-                familyMemberService.findByFamilyIdAndMemberId(family1.getId(),member2.getId()).get();
-
-        //then
-        assertEquals("nameFm2",find.getName());
+        assertEquals(2,response.size());
     }
 
     @Test
     void 가족구성원_정보_수정(){
         //given
-        Member member1 = getMember("email1@email.com");
-        Family family1 = getFamily("name1");
-        FamilyMember familyMember1 = getFamilyMember(member1, family1, "nameFm1");
+        IdResponse idResponse = getFamilyMember("name1",member1,family1);
+        ModifyInfoRequest modifyRequest = new ModifyInfoRequest();
+        modifyRequest.setName("modify");
+        modifyRequest.setProfileImg("img");
 
         //when
-        familyMemberService.modifyFamilyMemberInfo(familyMember1.getId(),"modify");
+        familyMemberService.modifyFamilyMemberInfo(idResponse.getId(),modifyRequest);
 
         //then
-        assertEquals("modify",familyMember1.getName());
+        FamilyMemberInfoResponse response = familyMemberService.getFamilyMemberInfoWithId(idResponse.getId());
+        assertEquals(response.getName(),"modify");
+    }
+    private IdResponse getFamilyMember(
+            String name, Member member, Family family
+    ) {
+        familyRepository.save(family);
+        memberRepository.save(member);
+
+        SaveRequest request = new SaveRequest();
+        request.setName(name);
+        request.setMemberId(member.getId());
+        request.setProfileImg("img");
+
+        return familyMemberService.saveFamilyMember(request,family.getId());
     }
 
-    //=CreateMethod==//
-    private Family getFamily(String name) {
-        Family family = Family.builder().name(name).build();
-        familyService.save(family);
-        return family;
-    }
-    private Member getMember(String email) {
-        Member member = Member.builder()
-                .authority(Authority.ROLE_USER)
-                .email(email)
-                .password("password")
-                .build();
-        memberService.join(member);
-        return member;
-    }
-    private FamilyMember getFamilyMember(Member member, Family family, String nameFm) {
-        FamilyMember familyMember =
-                FamilyMember.builder().member(member).family(family).name(nameFm).profileImg("profileImg").build();
-        familyMemberService.save(familyMember);
-        return familyMember;
-    }
 }
